b89d7845fcc7880b02b641439c4d51f2abe949ca
Maros Kollar <maros@k-1.com>
2015-10-09 09:17:18 +0200

    Module WIP

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ Version: 1.0.0
 -----------------------------------------------------------------------------
 Author: maros@k-1.com <maros@k-1.com>
 Description:
-    Sends sensor stats to InfluxDB
+    Collects sensor stats in an InfluxDB
+
 ******************************************************************************/
 
 // ----------------------------------------------------------------------------
@@ -29,18 +30,29 @@ InfluxDbStats.prototype.init = function (config) {
     InfluxDbStats.super_.prototype.init.call(this, config);
     
     var self = this;
+    self.callbacks = {};
     
-    this.controller.devices.on(this.config.sensor, 'change:metrics:level', function() {
-    	self.checkTemp();
+    _.each(self.config.devices,function(deviceId){
+        // Build, register and call check callback
+        var callback = _.bind(self.updateDevice,self,deviceId);
+        self.callbacks[deviceId] = deviceObject.on('change:metrics:level',callback);
     });
+    
+    this.timer = setInterval(_.bind(self.updateAll,self), 60*60*1000);
+    //self.updateCalculation();
 };
 
 InfluxDbStats.prototype.stop = function () {
     var self = this;
 
-    this.controller.devices.off(this.config.sensor, 'change:metrics:level', function() {
-    	self.checkTemp();
+    _.each(self.callbacks,function(deviceId,callbackFunction){
+        // Build, register and call check callback
+        var device  = self.controller.devices.get(deviceId);
+        device.off('change:metrics:level',callbackFunction);
     });
+    self.callbacks = {};
+    
+    clearTimeout(self.timer);
 
     InfluxDbStats.super_.prototype.stop.call(this);
 };
@@ -49,5 +61,12 @@ InfluxDbStats.prototype.stop = function () {
 // --- Module methods
 // ----------------------------------------------------------------------------
 
-InfluxDbStats.prototype.sendStats = function () {
+InfluxDbStats.prototype.updateDevice = function (deviceId) {
+    //escape comma and space
+    //id,room=serverA,tag=value level=XXX
 }
+
+
+InfluxDbStats.prototype.sendStats = function () {
+    // http request
+};
